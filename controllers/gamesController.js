69220f452aae8ec2dca91134a00ced50a6c1d278
Kethlyn <kethlynsaraa@gmail.com>
2022-05-28 22:42:22 -0300

    :sparkles: feat: get games

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,5 +1,28 @@
 import connection from '../database.js';
 
+export async function getAllGames(req, res) {
+    const { name } = req.query;
+    try {
+        if (name) {
+            const games = await connection.query(`
+            SELECT games.*, categories.name as "categoryName"
+            FROM games
+            JOIN categories ON games."categoryId" = categories.id
+            WHERE lower(games.name) LIKE '%${name}%';`);
+            res.status(200).send(games.rows);
+        } 
+        else {
+            const allGames = await connection.query(`
+            SELECT games.*, categories.name as "categoryName"
+            FROM games
+            JOIN categories ON games."categoryId" = categories.id;`);
+            res.status(200).send(allGames.rows);
+        }        
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+
 export async function addGame(req, res) {
     const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
     try {
--- routers/gamesRouter.js
+++ routers/gamesRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { addGame } from '../controllers/gamesController.js';
+import { addGame, getAllGames } from '../controllers/gamesController.js';
 import { gamesMiddleware } from '../middlewares/gamesMiddleware.js';
 
 const gamesRouter = Router();
 
+gamesRouter.get('/games', getAllGames);
 gamesRouter.post('/games', gamesMiddleware, addGame);
 
 export default gamesRouter;
