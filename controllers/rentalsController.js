3c0833768d5aa4a2621bb4373950197d4e163ed6
Kethlyn <kethlynsaraa@gmail.com>
2022-05-29 18:01:22 -0300

    :sparkles: feat: get rentals

--- controllers/rentalsController.js
+++ controllers/rentalsController.js
@@ -2,6 +2,69 @@ import dayjs from 'dayjs';
 
 import connection from '../database.js';
 
+export async function getAllRentals(req, res) {
+    const { customerId, gameId } = req.query;
+
+    try {
+        let getRentals = null;
+        if (customerId) {
+            getRentals = await connection.query(`
+            SELECT rentals.*, customers.name as "customerName", games.name as "gameName", games."categoryId", categories.name as "categoryName"
+            FROM rentals
+            JOIN customers ON rentals."customerId" = customers.id
+            JOIN games ON games.id = rentals."gameId"
+            JOIN categories ON categories.id = games."categoryId"
+            WHERE rentals."customerId" = $1 
+            `, [customerId]);    
+        } else if (gameId) {
+            getRentals = await connection.query(`
+            SELECT rentals.*, customers.name as "customerName", games.name as "gameName", games."categoryId", categories.name as "categoryName"
+            FROM rentals
+            JOIN customers ON rentals."customerId" = customers.id
+            JOIN games ON games.id = rentals."gameId"
+            JOIN categories ON categories.id = games."categoryId"
+            WHERE rentals."gameId" = $1 
+            `, [gameId]);    
+        } else {
+            getRentals = await connection.query(`
+            SELECT rentals.*, customers.name as "customerName", games.name as "gameName", games."categoryId", categories.name as "categoryName"
+            FROM rentals
+            JOIN customers ON rentals."customerId" = customers.id
+            JOIN games ON games.id = rentals."gameId"
+            JOIN categories ON categories.id = games."categoryId"
+            `);
+        }
+
+        const rentals = getRentals.rows;
+
+        const sendRentals = [];
+
+        for (let rental of rentals) {
+            rental = {
+                ...rental,
+                customer: {
+                    id: rental.customerId,
+                    name: rental.customerName
+                },
+                game: {
+                    id: rental.gameId,
+                    name: rental.gameName,
+                    categoryId: rental.categoryId,
+                    categoryName: rental.categoryName
+                }
+            }
+            delete rental.customerName;
+            delete rental.gameName;
+            delete rental.categoryId
+            delete rental.categoryName
+            sendRentals.push(rental);        
+        }
+        res.status(200).send(sendRentals);
+    } catch (e) {
+        res.sendStatus(500);
+    }
+}
+
 export async function postRental(req, res) {
     const { customerId, gameId, daysRented } = req.body;
     const { pricePerDay } = res.locals;
--- routers/rentalsRouter.js
+++ routers/rentalsRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { postRental } from '../controllers/rentalsController.js';
+import { getAllRentals, postRental } from '../controllers/rentalsController.js';
 import { rentalValidate } from '../middlewares/rentalMiddleware.js';
 
 const rentalsRouter = Router();
 
+rentalsRouter.get('/rentals', getAllRentals)
 rentalsRouter.post('/rentals', rentalValidate, postRental);
 
 export default rentalsRouter;
